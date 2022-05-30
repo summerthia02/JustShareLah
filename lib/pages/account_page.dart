ab6e48676df4ae54fee5dee27a3bc626366f4a12
Praveen Krishna <praveenkrishna0512@gmail.com>
2022-05-30 19:19:41 +0800

    Added account_page.dart

--- lib/pages/account_page.dart
+++ lib/pages/account_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:justsharelah_v1/apptheme.dart';
 import 'package:supabase/supabase.dart';
 import 'package:justsharelah_v1/components/auth_required_state.dart';
 import 'package:justsharelah_v1/utils/constants.dart';
@@ -11,9 +12,31 @@ class AccountPage extends StatefulWidget {
 }
 
 class _AccountPageState extends AuthRequiredState<AccountPage> {
-  final _usernameController = TextEditingController();
-  final _websiteController = TextEditingController();
   var _loading = false;
+  // Index for bottom nav bar
+  int _selectedIndex = 0;
+
+  // Routing for bottom nav bar
+  void _onItemTapped(int index) {
+    setState(() {
+      _loading = true;
+    });
+    switch (index) {
+      case 0:
+        // _navigatorKey.currentState!.pushNamed("/chat");
+        break;
+      case 1:
+        // _navigatorKey.currentState!.pushNamed("/addlisting");
+        break;
+      case 2:
+        Navigator.of(context).pushNamed("/account");
+        break;
+    }
+    setState(() {
+      _selectedIndex = index;
+      _loading = false;
+    });
+  }
 
   /// Called once a user id is received within `onAuthenticated()`
   Future<void> _getProfile(String userId) async {
@@ -84,35 +107,65 @@ class _AccountPageState extends AuthRequiredState<AccountPage> {
 
   @override
   void dispose() {
-    _usernameController.dispose();
-    _websiteController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Profile')),
-      body: ListView(
-        padding: const EdgeInsets.symmetric(vertical: 18, horizontal: 12),
-        children: [
-          TextFormField(
-            controller: _usernameController,
-            decoration: const InputDecoration(labelText: 'User Name'),
-          ),
-          const SizedBox(height: 18),
-          TextFormField(
-            controller: _websiteController,
-            decoration: const InputDecoration(labelText: 'Website'),
-          ),
-          const SizedBox(height: 18),
-          ElevatedButton(
-              onPressed: _updateProfile,
-              child: Text(_loading ? 'Saving...' : 'Update')),
-          const SizedBox(height: 18),
-          ElevatedButton(onPressed: _signOut, child: const Text('Sign Out')),
-        ],
+      appBar: AppBar(
+        title: const Text('Profile'),
+        centerTitle: true,
       ),
-    );
+        body: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 18, horizontal: 12),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const SizedBox(height: 64),
+              const Icon(Icons.face_rounded),
+              const SizedBox(height: 12),
+              Center(
+                child: Text(
+                  "Your Username",
+                  style: TextStyle(
+                    fontSize:
+                        Theme.of(context).textTheme.headline6?.fontSize ?? 32)
+                ),
+              ),
+              const Expanded(child: SizedBox(height: 18)),
+              ElevatedButton(
+                onPressed: _signOut,
+                child: const Text('Sign Out')
+              ),
+              const SizedBox(height: 8),
+              const Center(
+                child: Text("*Button to be removed subsequently"),
+              ),
+              const SizedBox(height: 64),
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedIndex,
+          onTap: _loading ? null : _onItemTapped,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.chat),
+              label: 'Chat',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.add),
+              label: 'Add Listing',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.face_outlined),
+              label: 'User Profile',
+            ),
+          ],
+        ),
+      );
   }
 }
