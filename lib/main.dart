abc9834b3bc5aa1a2f4ca72524f138078d1469f9
Praveen Krishna <praveenkrishna0512@gmail.com>
2022-06-21 22:55:06 +0800

    Configured Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:justsharelah_v1/apptheme.dart';
 import 'package:justsharelah_v1/pages/addListing.dart';
@@ -5,21 +6,16 @@ import 'package:justsharelah_v1/pages/feed_page.dart';
 import 'package:justsharelah_v1/pages/forget_password.dart';
 import 'package:justsharelah_v1/pages/signup_page.dart';
 import 'package:justsharelah_v1/profile_page.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
-import 'package:justsharelah_v1/pages/account_page.dart';
 import 'package:justsharelah_v1/pages/login_page.dart';
 import 'package:justsharelah_v1/pages/splash_page.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:justsharelah_v1/pages/chat_page.dart';
+
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  // TODO: Use .env
-  await Supabase.initialize(
-    url: 'https://etegbwhzssurytyojhtf.supabase.co',
-    anonKey:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV0ZWdid2h6c3N1cnl0eW9qaHRmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTMyMTIyMjEsImV4cCI6MTk2ODc4ODIyMX0.AbyAtt9P8DPc1MuvfAaPmZ03xI9LjA4L1jc3NZujPbU',
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(MyApp());
 }
