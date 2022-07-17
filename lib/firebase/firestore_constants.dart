ed54f9ecd725f41c816d0084cf347876fd6063b6
Praveen Krishna <praveenkrishna0512@gmail.com>
2022-07-17 14:44:40 +0800

    Made firestore_constants.dart and ChatMessage.dart

---
+++ lib/firebase/firestore_constants.dart
@@ -0,0 +1,7 @@
+class FirestoreChatConstants {
+  static const String idFrom = "idFrom";
+  static const String idTo = "idTo";
+  static const String timestamp = "timestamp";
+  static const String content = "content";
+  static const String type = "type";
+}
